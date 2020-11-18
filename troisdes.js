5407a738e8a87d4c7f42e012b47dfc9bb7419663
Romain heloise <romheloise@gmail.com>
2020-11-18 21:06:32 +0100

    text following mouse

--- troisdes.js
+++ troisdes.js
@@ -2,7 +2,10 @@ import * as THREE from "../node_modules/three/build/three.module.js";
 
 // CREATE CANVAS && RENDERER //
 const canvas = document.querySelector("canvas");
-const renderer = new THREE.WebGL1Renderer({ canvas });
+const renderer = new THREE.WebGL1Renderer({
+  canvas,
+  antialias: true,
+});
 
 // CREATE CAMERA AND SET POSITION //
 const fov = 40;
@@ -25,22 +28,23 @@ scene.add(light); // add light to scene
 renderer.setClearColor("white");
 
 // CREATE CUBE //
-const createCube = (x, y) => {
+const createCube = (x, y, z) => {
   const texture = new THREE.TextureLoader().load(
     "./Metal_Mesh_003_basecolor.jpg"
   );
-  const material = new THREE.MeshPhongMaterial({ color: 'blue' });
+  const material = new THREE.MeshPhongMaterial({ color: "blue" });
   const geometry = new THREE.BoxGeometry(8, 8, 8);
   const box = new THREE.Mesh(geometry, material);
   box.position.x = x;
   box.position.y = y;
+  box.position.z = z;
   scene.add(box);
   modeles.push(box);
   return box;
 };
 
 //CREATE DECA //
-const deca = (x, y,z) => {
+const deca = (x, y, z) => {
   const material = new THREE.MeshToonMaterial({ color: "orange" });
   const radius = 5;
   const detail = 0;
@@ -54,12 +58,12 @@ const deca = (x, y,z) => {
   return deca;
 };
 
-// CREATE TEXT // 
+// CREATE TEXT //
 
 const textRota = (x, y, z) => {
   const loader = new THREE.FontLoader();
-  loader.load('./Pacifico_Regular.json', (font) => {
-    const text = 'Romain Heloise';
+  loader.load("./Pacifico_Regular.json", (font) => {
+    const text = "Romain Heloise";
     const geometry = new THREE.TextBufferGeometry(text, {
       font: font,
       size: 5,
@@ -70,28 +74,30 @@ const textRota = (x, y, z) => {
       bevelSize: 0.3,
       bevelSegments: 5,
     });
+    geometry.applyMatrix( new THREE.Matrix4().makeTranslation(x, y, z) );
     const material = new THREE.MeshToonMaterial({ color: "red" });
     const textRen = new THREE.Mesh(geometry, material);
     textRen.position.x = x;
     textRen.position.y = y;
     textRen.position.z = z;
     textRen.rotation.x += 0.2;
-    scene.add(textRen)
+    blaze.push(textRen);
+    scene.add(textRen);
   });
-
-
-}
-
-
+};
 
 // RENDERING //
 const renderScene = () => {
-
   requestAnimationFrame(renderScene);
 
   modeles.forEach((modele) => {
     modele.rotation.x += 0.01;
     modele.rotation.y += 0.01;
+  });
+
+  blaze.forEach((x) => {
+    x.rotation.y = (mouseCoord[0] - window.innerWidth / 2 ) / (window.innerWidth*4);
+    x.rotation.x = (mouseCoord[1] - window.innerHeight/ 2 ) / (window.innerHeight*4);
   });
 
   const vec = new THREE.Vector2(0, 1);
@@ -102,11 +108,7 @@ const renderScene = () => {
     canvas.clientHeight * pixelRatio !== height ||
     canvas.clientWidth * pixelRatio !== width;
   if (needResize) {
-    renderer.setSize(
-      canvas.clientWidth * pixelRatio,
-      canvas.clientHeight * pixelRatio,
-      false
-    );
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
   }
 
   camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -116,8 +118,15 @@ const renderScene = () => {
 };
 
 const modeles = [];
-textRota(-25, 10, 3)
-const cube = createCube(-10, 8);
-const decaH = deca(20, 15,25);
+const blaze = [];
+textRota(-15, 10, 3);
+const cube = createCube(-10, 8,-15);
+const decaH = deca(20, 20, 25);
+const mouseCoord = [0, 0];
+
+window.addEventListener("mousemove", (e) => {
+  mouseCoord[0] = e.clientX;
+  mouseCoord[1] = e.clientY;
+});
 
 renderScene();
